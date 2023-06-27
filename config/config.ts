235557f2af52f0adfb71a96443c016c144f6210c
wminaung <minwin243@gmail.com>
2023-06-27 18:11:04 +0630

    delete .env

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,12 @@
 interface Config {
   apiBaseUrl: string;
+  googleClientId: string;
+  googleClientSecret: string;
+  nextAuthSecret: string;
 }
 export const config: Config = {
   apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || "",
+  googleClientId: process.env.GOOGLE_CLIENT_ID || "",
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+  nextAuthSecret: process.env.NEXTAUTH_SECRET || "",
 };
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,16 +1,30 @@
-import NextAuth from "next-auth";
+import { config } from "@/config/config";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID || "",
-      clientSecret: process.env.GOOGLE_SECRET || "",
+      clientId: config.googleClientId,
+      clientSecret: config.googleClientSecret,
     }),
     // ...add more providers here
   ],
-  secret: process.env.NEXTAUTH_SECRET || "",
+  secret: config.nextAuthSecret,
+  pages: {
+    signIn: "/auth/login",
+  },
 };
 
 export default NextAuth(authOptions);
+
+//*** middleware.ts */
+
+/*
+export { default } from "next-auth/middleware";
+export const config = {
+  matcher: ["/backoffice/:apth*", "/api/backoffice/:path*"],
+};
+
+*/
